a458467e20cdfe48481f532bc7da952a3e340184
burgokmen <burgokmen@gmail.com>
2023-09-06 16:39:22 +0300

    init + EditMovie connected to App

--- src/components/EditMovieForm.js
+++ src/components/EditMovieForm.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const EditMovieForm = (props) => {
   const { push } = useHistory();
+  let { id } = useParams();
 
   const { setMovies } = props;
   const [movie, setMovie] = useState({
@@ -42,7 +43,9 @@ const EditMovieForm = (props) => {
     <div className="bg-white rounded-md shadow flex-1">
       <form onSubmit={handleSubmit}>
         <div className="p-5 pb-3 border-b border-zinc-200">
-          <h4 className="text-xl font-bold">Düzenleniyor <strong>{movie.title}</strong></h4>
+          <h4 className="text-xl font-bold">
+            Düzenleniyor <strong>{movie.title}</strong>
+          </h4>
         </div>
 
         <div className="px-5 py-3">
